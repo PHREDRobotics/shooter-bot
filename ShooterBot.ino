f43efee8d30f2e8f7fa2f7f0009f9b546ac5d33f
Steven King <steveking16@outlook.com>
2025-07-26 15:59:29 -0700

    Made wheels move! :sparkles:

--- ShooterBot.ino
+++ ShooterBot.ino
@@ -33,11 +33,17 @@ void setup() {
     delay(1000);
   }
 
-  frontLeft.attach(13);
+  frontLeft.attach(8);
   frontRight.attach(12);
   backLeft.attach(11);
   backRight.attach(10);
   cannon.attach(9);
+
+  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
 
   frontLeft.writeMicroseconds(1500);
   frontRight.writeMicroseconds(1500);
@@ -54,31 +60,49 @@ void loop() {
   RY = ps2x.Analog(PSS_RY);
   RX = ps2x.Analog(PSS_RX);
 
-  drive(LY, LX, RX);
+  drive(LY, 127, 127);
+  
+  if (ps2x.Button(PSB_GREEN)) {
+    shoot(0.5);
+  }
 
-  if (ps2x.Button(PSB_GREEN) && (launch_windup + launch_increment) < 1) {
+  /*if (ps2x.Button(PSB_GREEN) && (launch_windup + launch_increment) < 1) {
     launch_windup += launch_increment;
   } else if (launch_windup > 0) {
-    shoot(launch_windup);
+    shoot(launch_windup * 127);
     launch_windup = 0;
-  }
+  } */
 }
 
-void set(Servo motor, float power) {
-  motor.writeMicroseconds(map(power, -1, 1, 1000, 2000));
+void setFrontLeft(float power) {
+  frontLeft.writeMicroseconds(map(power, 0, 255, 1700, 1300));
+}
+void setFrontRight(float power) {
+  frontRight.writeMicroseconds(map(power, 0, 255, 1300, 1700));
+}
+void setBackLeft(float power) {
+  backLeft.writeMicroseconds(map(power, 0, 255, 1700, 1300));
+}
+void setBackRight(float power) {
+  backRight.writeMicroseconds(map(power, 0, 255, 1300, 1700));
 }
 
-void drive(float x, float y, float theta) {
-  set(frontLeft, x + y + theta);
+void drive(float drive, float strafe, float turn) {
+  /*set(frontLeft, x + y + theta);
   set(frontRight, x - y - theta);
   set(backLeft, x - y + theta);
-  set(backRight, x + y - theta);
+  set(backRight, x + y - theta);*/
+
+  setFrontLeft(drive);
+  setFrontRight(drive);
+  setBackLeft(drive);
+  setBackRight(drive);
 }
 
 void shoot(float power) {
-  set(cannon, power);
+  cannon.writeMicroseconds(map(power, 0, 1, 1000, 2000));
 
   delay(1000);
 
-  set(cannon, 0);
+  cannon.writeMicroseconds(1500);
 }
